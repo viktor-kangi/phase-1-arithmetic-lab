0b2daa90ebbbe9ce07d0a83f37e9b2664f982ba5
victor <victor.buthia@student.moringaschool.com>
2022-11-28 13:11:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+/*const multiply = (num1, num2) => {
+    return num1 * num2;
+  };
+  let resulMultiply = multiply(31, 2);
+  console.log(resulMultiply);*/
+
+  const num1 = 31
+  const num2 = 2
+  const multiply = num1 * num2
+
+  const random = Math.ceil(Number.isInteger(2))
+
+  const num3 = 32
+  const num4 = 7
+  const mod = num3 % num4
+
+  const max = Math.max(1,3,15,12,15,20)
+
+    
